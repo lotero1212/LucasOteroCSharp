c7ce53dda6543bd49a17c68bb6eb1e2a48f8a5ec
Lucas Otero <lucasotero.ag@gmail.com>
2022-07-20 09:33:56 -0300

    Add project files.

---
+++ LucasOteroCSharp/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace LucasOteroCSharp
+{ 
+
+    public class Usuario
+    {
+
+        private short id;
+        private string nombre;
+        private string apellido;
+        private string nombreUsuario;
+        private string contraseña;
+        private string mail;
+
+    }
+
+    public class Producto
+    {
+
+        private short id;
+        private string descripcion;
+        private double costo;
+        private double precioVenta;
+        private int stock;
+        private short idUsuario;
+
+    }
+
+    public class ProductoVendido
+    {
+
+        private short id;
+        private short idProducto;
+        private int stock;
+        private short idVenta;
+
+    }
+    public class Venta
+    {
+
+        private short id;
+        private string comentarios;
+
+    }
+
+}
